6af273382642ab6ee0434e720b103361cfe1ab14
pro-streetdancer <156317306+pro-streetdancer@users.noreply.github.com>
2025-04-04 18:16:33 +0100

    Add update checker

--- slim-seo-schema/slim-seo-schema.php
+++ slim-seo-schema/slim-seo-schema.php
@@ -39,3 +39,19 @@ add_action( 'plugins_loaded', function () {
 	}
 	require __DIR__ . '/bootstrap.php';
 } );
+
+
+require 'plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/pro-streetdancer/tiny-seo-schema/',
+	__FILE__,
+	'tiny-seo-schema'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+//$myUpdateChecker->setAuthentication('your-token-here');
