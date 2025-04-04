127b36f0f43a71f272b6d4dce2f83cecf8011dc0
pro-streetdancer <156317306+pro-streetdancer@users.noreply.github.com>
2025-04-04 18:13:09 +0100

    License Changes

--- slim-seo-schema/slim-seo-schema.php
+++ slim-seo-schema/slim-seo-schema.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * Plugin Name: Slim SEO Schema
- * Plugin URI:  https://wpslimseo.com
- * Description: A schema builder plugin for WordPress.
- * Author:      eLightUp
- * Author URI:  https://elightup.com
+ * Plugin Name: Tiny SEO Schema
+ * Description: A modified schema builder plugin for WordPress.
+ * Author:      Anon
  * Version:     2.7.1
  * Text Domain: slim-seo-schema
  * Domain Path: /languages/
